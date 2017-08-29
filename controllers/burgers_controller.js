10a817f848cc58e573ceb2c03069ba105380f3df
David Lac <kiddmit3@gmail.com>
2017-08-29 00:09:41 -0700

    working

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get('/', function(req, res){
+  res.redirect('/burgers')
+});
+
+router.get('/burgers', function(req, res){
+  burger.selectAll(function(data){
+    var hbsObject = {burgers: data};
+
+    console.log(hbsObject);
+
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/burgers/create', function(req, res){
+  burger.insertOne(['burger_name'], [req.body.b_name], function(data){
+    res.redirect('/burgers')
+  });
+});
+
+router.put('/burgers/update/:id', function(req, res){
+  var condition = 'id = ' + req.params.id;
+
+  console.log('condition ', condition);
+
+  burger.updateOne({'devoured': req.body.devoured}, condition, function(data){
+    res.redirect('/burgers');
+  });
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
+
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("McDouble",0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("McChicken",0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Bob's Special",1);
